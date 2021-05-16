e4a16bcfc91dc773bfcc5a1212404f9c0a158ec2
zakariya <97zakariyameraj@gmail.com>
2021-05-16 20:19:36 +0000

    hi

--- src/core_java/Main.java
+++ src/core_java/Main.java
@@ -2,7 +2,7 @@ package core_java;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world how are u doing");
 	}
 
 }
